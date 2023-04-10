d3952fd12c7725ec7dd8d6cf8be1da9b25b8b183
Shaun Prince <suparious@solidrust.net>
2023-04-10 15:46:32 -0700

    download and build llama.cpp if it is missing

--- chat.py
+++ chat.py
@@ -4,21 +4,30 @@ import os
 import sys
 import subprocess
 
+def download_and_build_llama(llama_path):
+  # Clone the llama.cpp repository
+  subprocess.run(['git', 'clone', 'https://github.com/ggerganov/llama.cpp.git', llama_path])
+  # Build the llama.cpp executable
+  subprocess.run(['make'], cwd=llama_path)
+
 def main():
   # Set the model path. Use the provided argument, if available, otherwise use the default model path.
   model = sys.argv[1] if len(sys.argv) > 1 else os.path.join(os.environ['HOME'], 'models', 'gpt4-x-alpaca-native-13B-ggml.bin')
   # Set the prompt path. Use the provided argument, if available, otherwise use the default prompt path.
   prompt = sys.argv[2] if len(sys.argv) > 1 else os.path.join(os.environ['HOME'], 'llama-ggml-tools', 'prompts', 'Alpaca.txt')
   # Set the path for the llama.cpp repository.
-  llama = os.path.join(os.environ['HOME'], 'llama-ggml-tools', 'repos', 'llama.cpp')
-  # If the llama.cpp folder is missing, download it from https://github.com/ggerganov/llama.cpp
-  # Run `make` in the llama.cpp folder to build the executable
+  llama_path = os.path.join(os.environ['HOME'], 'llama-ggml-tools', 'repos', 'llama.cpp')
+  llama_executable = os.path.join(llama_path, 'main')
+  # Check if the llama.cpp folder exists
+  if not os.path.exists(llama_path):
+    print("llama.cpp folder not found. Downloading and building the executable...")
+    download_and_build_llama(llama_path)
 
   print(f"Model: {model}")
 
   # Run the llama.cpp executable with the specified arguments
   subprocess.run([
-    os.path.join(llama, 'main'),
+    llama_executable,
     '-m', model,
     '--color',
     '-f', os.path.join(prompt),

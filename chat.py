f53c1529fdc2d1f861e6ab930b06476f50ed6eff
Shaun Prince <suparious@solidrust.net>
2023-04-10 16:05:28 -0700

    add some messaging around the expected usage

--- chat.py
+++ chat.py
@@ -4,6 +4,11 @@ import os
 import sys
 import subprocess
 
+def print_usage():
+  print("Usage: python3 chat.py [model_path] [prompt_path]")
+  print("  model_path: Optional. Path to the model file. Defaults to the gpt4-x-alpaca-native-13B-ggml.bin in the 'llama-ggml-tools/models' directory in the user's home.")
+  print("  prompt_path: Optional. Path to the prompt file. Defaults to the Alpaca.txt in the 'llama-ggml-tools/prompts' directory in the user's home.")
+
 def download_and_build_llama(llama_path):
   # Clone the llama.cpp repository
   subprocess.run(['git', 'clone', 'https://github.com/ggerganov/llama.cpp.git', llama_path])
@@ -12,16 +17,18 @@ def download_and_build_llama(llama_path):
 
 def main():
   # Set the model path. Use the provided argument, if available, otherwise use the default model path.
-  model = sys.argv[1] if len(sys.argv) > 1 else os.path.join(os.environ['HOME'], 'models', 'gpt4-x-alpaca-native-13B-ggml.bin')
+  model = sys.argv[1] if len(sys.argv) > 1 else os.path.join(os.environ['HOME'], 'llama-ggml-tools', 'models', 'gpt4-x-alpaca-native-13B-ggml.bin')
   # Check if the model file exists
   if not os.path.isfile(model):
     print(f"Error: Model file not found at {model}")
+    print_usage()
     sys.exit(1)
   # Set the prompt path. Use the provided argument, if available, otherwise use the default prompt path.
   prompt = sys.argv[2] if len(sys.argv) > 1 else os.path.join(os.environ['HOME'], 'llama-ggml-tools', 'prompts', 'Alpaca.txt')
   # Check if the prompt file exists
   if not os.path.isfile(prompt):
     print(f"Error: Prompt file not found at {prompt}")
+    print_usage()
     sys.exit(1)
   # Set the path for the llama.cpp repository.
   llama_path = os.path.join(os.environ['HOME'], 'llama-ggml-tools', 'repos', 'llama.cpp')

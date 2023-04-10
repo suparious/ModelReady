0f3ce124bd88b1bf9b371eb99d7d9f5d6612c999
Shaun Prince <suparious@solidrust.net>
2023-04-10 15:37:20 -0700

    adding some helpful comments

--- chat.py
+++ chat.py
@@ -7,13 +7,16 @@ import subprocess
 def main():
   # Set the model path. Use the provided argument, if available, otherwise use the default model path.
   model = sys.argv[1] if len(sys.argv) > 1 else os.path.join(os.environ['HOME'], 'models', 'gpt4-x-alpaca-native-13B-ggml.bin')
+  # Set the prompt path. Use the provided argument, if available, otherwise use the default prompt path.
   prompt = sys.argv[2] if len(sys.argv) > 1 else os.path.join(os.environ['HOME'], 'llama-ggml-tools', 'prompts', 'Alpaca.txt')
+  # Set the path for the llama.cpp repository.
   llama = os.path.join(os.environ['HOME'], 'llama-ggml-tools', 'repos', 'llama.cpp')
-  # if missing the llama.cpp folder, download it from https://github.com/ggerganov/llama.cpp
-  # run `make` in the llama.cpp folder to build the executable
+  # If the llama.cpp folder is missing, download it from https://github.com/ggerganov/llama.cpp
+  # Run `make` in the llama.cpp folder to build the executable
 
   print(f"Model: {model}")
 
+  # Run the llama.cpp executable with the specified arguments
   subprocess.run([
     os.path.join(llama, 'main'),
     '-m', model,

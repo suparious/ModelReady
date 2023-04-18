8ed120ffb07b39dac5265e4195b6ab7af190019e
Shaun Prince <suparious@solidrust.net>
2023-04-18 10:27:13 -0700

    allow for a re-install / upgrade of ooba

--- installers/ooba.sh
+++ installers/ooba.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # This script is used to install the Ooba application
-XFORMERS_WHL=$1
+export XFORMERS_WHL=$1
 # Set the environment variables
 INSTALL_DIR="${INSTALL_DIR:-${HOME}/ooba}"
 CUDA_HOME="${CUDA_HOME:-/usr/local/cuda-11.7}"
@@ -22,17 +22,17 @@ fi
 
 # clone the repository
 progress "Cloning the Ooba repository..."
-git clone https://github.com/oobabooga/text-generation-webui.git "${INSTALL_DIR}"
-cd "${INSTALL_DIR}"
+if [ -d "${INSTALL_DIR}" ]; then
+  git clone https://github.com/oobabooga/text-generation-webui.git "${INSTALL_DIR}"
+else
+  cd "${INSTALL_DIR}"
+  git pull
+fi
 
 # create the virtual environment
 progress "Creating virtual python environment..."
 python -m venv venv
 source venv/bin/activate
-
-# install the matching cuda version of pytorch and torchvision
-progress "Installing PyTorch, TorchVision, and TorchAudio for CUDA ${CUDA_VERSION}..."
-pip install torch torchvision torchaudio --extra-index-url "https://download.pytorch.org/whl/cu${CUDA_VERSION}"
 
 # install the requirements
 progress "Installing other Python dependencies..."
@@ -49,10 +49,13 @@ mkdir -p "${INSTALL_DIR}/repositories"
 # Build custom pytorch
 if [ ${CUDA_VERSION} -gt 118 ]; then
   progress "Building custom PyTorch for CUDA ${CUDA_VERSION}..."
-  git clone --recursive https://github.com/pytorch/pytorch "${INSTALL_DIR}/repositories/pytorch"
+  if [ ! -d "${INSTALL_DIR}/repositories/pytorch" ]; then
+    git clone --recursive https://github.com/pytorch/pytorch "${INSTALL_DIR}/repositories/pytorch"
+  fi
   cd "${INSTALL_DIR}/repositories/pytorch"
   git submodule sync
   git submodule update --init --recursive
+  conda init
   conda update -n base -c defaults conda -y
   conda create -n torch-${CUDA_VERSION} -y
   conda activate torch-${CUDA_VERSION}
@@ -65,13 +68,18 @@ if [ ${CUDA_VERSION} -gt 118 ]; then
   python setup.py develop
   # What comes next?
   conda deactivate
+else
+  # install the matching cuda version of pytorch and torchvision
+  progress "Installing PyTorch, TorchVision, and TorchAudio for CUDA ${CUDA_VERSION}..."
+  pip install torch torchvision torchaudio --extra-index-url "https://download.pytorch.org/whl/cu${CUDA_VERSION}"
 fi
 
 # Install GPTQ support for 4bit 128g LLaMA
 progress "Installing GPTQ for LLaMA..."
-cd "${INSTALL_DIR}/repositories"
-git clone https://github.com/oobabooga/GPTQ-for-LLaMa.git -b cuda
-cd GPTQ-for-LLaMa
+if [ ! -d "${INSTALL_DIR}/repositories/GPTQ-for-LLaMa" ]; then
+  git clone https://github.com/oobabooga/GPTQ-for-LLaMa.git -b cuda "${INSTALL_DIR}/repositories/GPTQ-for-LLaMa"
+fi
+cd "${INSTALL_DIR}/repositories/GPTQ-for-LLaMa"
 pip install torch torchvision --extra-index-url "https://download.pytorch.org/whl/cu${CUDA_VERSION}"
 python setup_cuda.py install
 cd "${INSTALL_DIR}"
@@ -79,9 +87,11 @@ cd "${INSTALL_DIR}"
 # Install xformers for extra speed and memory efficiency
 if [ -z ${XFORMERS_WHL} ]; then
   progress "Building xformers (this can take a really long time)..."
-  cd "${INSTALL_DIR}/repositories"
-  git clone https://github.com/facebookresearch/xformers.git
-  cd xformers
+  if [ -d "${INSTALL_DIR}/repositories/xformers" ]; then
+    git clone https://github.com/facebookresearch/xformers.git "${INSTALL_DIR}/repositories/xformers"
+  fi
+  cd "${INSTALL_DIR}/repositories/xformers"
+  git pull
   git submodule update --init --recursive
   python -m venv venv
   source venv/bin/activate

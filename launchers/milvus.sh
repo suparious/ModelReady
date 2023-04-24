a4dd9c5cc79a4cb4530e21df39dac2d1361559c0
Shaun Prince <suparious@solidrust.net>
2023-04-24 13:14:31 -0700

    ensuring full POSIX compatability with BASH

--- launchers/milvus.sh
+++ launchers/milvus.sh
@@ -6,7 +6,7 @@ LISTEN_PORT="${LISTEN_PORT:-19530}"
 ## Use this if you have multiple milvus-proxy pods or a cluster setup
 #MILVUS_PROXY="$(kubectl get pod | grep milvus-proxy | grep Running | awk {' print $1 '})"
 ## Use this if you have a standalone milvus installation
-MILVUS_PROXY="$(kubectl get pod | grep ${RELEASE_NAME}-milvus | grep Running | awk {' print $1 '})"
+MILVUS_PROXY="$(kubectl get pod | grep "${RELEASE_NAME}-milvus" | grep Running | awk {' print $1 '})"
 MILVUS_PROXY_PORT="$(kubectl get pod ${MILVUS_PROXY} --template='{{(index (index .spec.containers 0).ports 0).containerPort}}{{"\n"}}')"
 DEPLOYMENT_CHECK="$(kubectl get svc | grep ${RELEASE_NAME} | grep ${MILVUS_PROXY_PORT})"
 
@@ -18,6 +18,6 @@ else
     echo "${RELEASE_NAME} not found"
   else
     echo "${RELEASE_NAME} appears deployed"
-    kubectl port-forward --address 0.0.0.0 service/${RELEASE_NAME}-milvus ${LISTEN_PORT}:${MILVUS_PROXY_PORT}
+    kubectl port-forward --address 0.0.0.0 "service/${RELEASE_NAME}-milvus" ${LISTEN_PORT}:${MILVUS_PROXY_PORT}
   fi
 fi

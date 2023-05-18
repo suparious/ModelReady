ef0339fabc46c1678dc6f55a71a91b34c5b0011b
Shaun Prince <suparious@solidrust.net>
2023-05-18 13:46:37 -0700

    simplify milvus status check

--- launchers/milvus.sh
+++ launchers/milvus.sh
@@ -9,7 +9,7 @@ fi
 
 # Start Milvus using Docker Compose
 cd "${INSTALL_DIR}"
-docker-compose up -d
+docker compose up -d
 
 # Get the name or ID of the Milvus Docker container
 #MILVUS_CONTAINER=$(docker-compose ps -q)
@@ -20,7 +20,7 @@ MILVUS_SERVICE="milvus"
 # Continuously check if the Milvus Docker container is running
 while true; do
   #if ! docker ps --format '{{.Names}}' | grep -q "^${MILVUS_CONTAINER}$"; then
-  if ! docker ps --format '{{.Names}}' | grep -q "^${MILVUS_SERVICE}_"; then
+  if ! docker ps --format '{{.Names}}' | grep "${MILVUS_SERVICE}"; then
     echo "Milvus Docker container has stopped."
     exit 1
   fi

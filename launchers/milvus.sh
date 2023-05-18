fd6621ac4118f0fc6e58779604c4fc167a2f7c01
Shaun Prince <suparious@solidrust.net>
2023-05-18 13:41:33 -0700

    update launcher to look for service name instead of ID

--- launchers/milvus.sh
+++ launchers/milvus.sh
@@ -12,11 +12,15 @@ cd "${INSTALL_DIR}"
 docker-compose up -d
 
 # Get the name or ID of the Milvus Docker container
-MILVUS_CONTAINER=$(docker-compose ps -q)
+#MILVUS_CONTAINER=$(docker-compose ps -q)
+
+# Define the name of the Milvus Docker service
+MILVUS_SERVICE="milvus"
 
 # Continuously check if the Milvus Docker container is running
 while true; do
-  if ! docker ps --format '{{.Names}}' | grep -q "^${MILVUS_CONTAINER}$"; then
+  #if ! docker ps --format '{{.Names}}' | grep -q "^${MILVUS_CONTAINER}$"; then
+  if ! docker ps --format '{{.Names}}' | grep -q "^${MILVUS_SERVICE}_"; then
     echo "Milvus Docker container has stopped."
     exit 1
   fi

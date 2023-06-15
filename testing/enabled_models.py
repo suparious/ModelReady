4ba18978fa13f36f468ecfb740fed41575b9187e
Shaun Prince <suparious@solidrust.net>
2023-06-15 15:18:51 -0700

    Update OpenAI available models to output alphabetically

--- testing/enabled_models.py
+++ testing/enabled_models.py
@@ -11,19 +11,19 @@ openai.api_key = config['openai_api_key']
 def get_models():
     try:
         models = openai.Model.list()
-        available_models = [model.id for model in models["data"]]
+        available_models = sorted([model.id for model in models["data"]])  # Sort the models alphabetically
         print("Models available to you:")
         for model in available_models:
             print(f"- {model}")
 
-        required_models = [
+        required_models = sorted([  # Sort the required models alphabetically
             "gpt-4",
             "gpt-4-0314",
             "gpt-4-32k",
             "gpt-4-32k-0314"
-        ]
+        ])
 
-        missing_models = [model for model in required_models if model not in available_models]
+        missing_models = sorted([model for model in required_models if model not in available_models])  # Sort the missing models alphabetically
         if missing_models:
             print("\nYou are missing access to the following models:")
             for model in missing_models:
@@ -35,5 +35,6 @@ def get_models():
         print(f"Error: {e}")
         print("Unable to retrieve model information.")
 
+
 if __name__ == "__main__":
-    get_models()%
+    get_models()
